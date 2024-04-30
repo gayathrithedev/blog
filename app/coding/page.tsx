19d04a709334a555e6e488059168f32eb57d026e
Gayathri Perumal <gayathriperumal100@gmail.com>
2024-04-30 16:44:32 +0530

    Fetch posts using infinite query

--- app/coding/page.tsx
+++ app/coding/page.tsx
@@ -1,9 +1,38 @@
+"use client";
+import PostList from "@/components/post-list";
 import { title } from "@/components/primitives";
+import { getPosts } from "@/lib/requests";
+import { Button } from "@nextui-org/button";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
-export default function AboutPage() {
+export default function CodingPage() {
+	const { data, hasNextPage, fetchNextPage, isFetching } = useInfiniteQuery({
+		queryKey: ["posts"],
+		queryFn: getPosts,
+		getNextPageParam: (lastPage) =>
+		  lastPage.length < 9 ? undefined : lastPage[lastPage.length - 1].cursor,
+		initialPageParam: "",
+	  });
+
 	return (
 		<div>
-			<h1 className={title()}>About</h1>
+			<h1 className={title({color: 'cyan'})}>Read my latest articles...</h1>
+			<PostList data={data} />
+			
+      		<div className="col-span-1 lg:col-span-3 w-full flex justify-center my-5">
+				<Button
+					className="w-full"
+					variant="bordered"
+					disabled={!hasNextPage || isFetching}
+					onClick={() => fetchNextPage()}
+				>
+				{isFetching
+					? "Loading..."
+					: hasNextPage
+					? "Load more"
+					: "That's all for today!"}
+				</Button>
+      		</div>
 		</div>
 	);
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,13 @@ import { Providers } from "./providers";
 import { Navbar } from "@/components/navbar";
 import { Link } from "@nextui-org/link";
 import clsx from "clsx";
+import { getPosts } from "@/lib/requests";
+import { PostMetadata } from "@/lib/types";
+import {
+	HydrationBoundary,
+	QueryClient,
+	dehydrate,
+  } from "@tanstack/react-query";
 
 export const metadata: Metadata = {
 	title: {
@@ -25,11 +32,26 @@ export const viewport: Viewport = {
 	],
 }
 
-export default function RootLayout({
+export default async function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
+	const queryClient = new QueryClient();
+
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: ["posts"],
+    queryFn: getPosts,
+    getNextPageParam: (
+      lastPage: {
+        node: PostMetadata;
+        cursor: string;
+      }[]
+    ) =>
+      lastPage.length < 9 ? undefined : lastPage[lastPage.length - 1].cursor,
+    initialPageParam: "",
+  });
+
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<head />
@@ -39,25 +61,27 @@ export default function RootLayout({
 					fontSans.variable
 				)}
 			>
-				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-					<div className="relative flex flex-col h-screen">
-						<Navbar />
-						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-							{children}
-						</main>
-						<footer className="w-full flex items-center justify-center py-3">
-							<Link
-								isExternal
-								className="flex items-center gap-1 text-current"
-								href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-								title="nextui.org homepage"
-							>
-								<span className="text-default-600">Powered by</span>
-								<p className="text-primary">NextUI</p>
-							</Link>
-						</footer>
-					</div>
-				</Providers>
+				<HydrationBoundary state={dehydrate(queryClient)}>
+					<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+						<div className="relative flex flex-col h-screen">
+							<Navbar />
+							<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+								{children}
+							</main>
+							<footer className="w-full flex items-center justify-center py-3">
+								<Link
+									isExternal
+									className="flex items-center gap-1 text-current"
+									href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
+									title="nextui.org homepage"
+								>
+									<span className="text-default-600">Powered by</span>
+									<p className="text-primary">NextUI</p>
+								</Link>
+							</footer>
+						</div>
+					</Providers>
+				</HydrationBoundary>
 			</body>
 		</html>
 	);
